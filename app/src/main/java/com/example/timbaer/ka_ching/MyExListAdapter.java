6029522feb0192075368aeeb315d445d46edfa89
Anshul <anshuls3@illinois.edu>
2018-12-03 02:58:14 -0600

    Made stuff visible

--- app/src/main/java/com/example/timbaer/ka_ching/MyExListAdapter.java
+++ app/src/main/java/com/example/timbaer/ka_ching/MyExListAdapter.java
@@ -1,26 +1,33 @@
 package com.example.timbaer.ka_ching;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.content.Context;
 import android.widget.TextView;
-
 import java.util.List;
 import java.util.Map;
+import android.graphics.Typeface;
+
 
 public class MyExListAdapter extends BaseExpandableListAdapter {
     Context context;
     List<String> companies;
     Map<String, List<String>> addInfo;
+    Typeface custom_font;
 
     public MyExListAdapter(Context context, List<String> companies, Map<String, List<String>> addInfo) {
         this.context = context;
         this.companies = companies;
         this.addInfo = addInfo;
+        try {
+            custom_font = Typeface.createFromAsset(context.getAssets(), "fonts/large_font.tff");
+        } catch (Exception e) {
+            Log.d("NoFont", e.toString());
+        }
     }
-
     @Override
     public int getGroupCount() {
         return companies.size();
@@ -66,6 +73,7 @@ public class MyExListAdapter extends BaseExpandableListAdapter {
         }
 
         TextView txtParent = (TextView) convertView.findViewById(R.id.txtParent);
+        //txtParent.setTypeface(custom_font);
         txtParent.setText(company);
         return convertView;
     }
